d87a59a86d717d5a0a4d23f83916ece00679282f
Liana <95958723+Lianatim@users.noreply.github.com>
2022-11-09 16:23:14 +0500

    3. Денежные переводы AccountStorage

---
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -0,0 +1,41 @@
+package ru.job4j.cash;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+public class AccountStorage {
+    private final HashMap<Integer, Account> accounts = new HashMap<>();
+
+    public synchronized boolean add(Account account) {
+        return accounts.putIfAbsent(account.id(), account) == null;
+    }
+
+    public synchronized boolean update(Account account) {
+        return accounts.replace(account.id(), accounts.get(account.id()), account);
+    }
+
+    public synchronized boolean delete(int id) {
+        return accounts.remove(id) != null;
+    }
+
+    public synchronized Optional<Account> getById(int id) {
+        Optional<Account> rsl = Optional.empty();
+        if (accounts.containsKey(id)) {
+            rsl = Optional.of(accounts.get(id));
+        }
+        return rsl;
+    }
+
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        boolean rsl = false;
+        String pattern = "Not found account by id = %d";
+        Account src = getById(fromId).orElseThrow(() -> new IllegalStateException(String.format(pattern, fromId)));
+        Account dest = getById(toId).orElseThrow(() -> new IllegalStateException(String.format(pattern, toId)));
+        if (src.amount() >= amount) {
+            update(new Account(fromId, src.amount() - amount));
+            update(new Account(toId, dest.amount() + amount));
+            rsl = true;
+        }
+        return rsl;
+    }
+}
---
+++ src/test/java/ru/job4j/cash/AccountStorageTest.java
@@ -0,0 +1,69 @@
+package ru.job4j.cash;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class AccountStorageTest {
+
+    @Test
+    void whenAdd() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 100));
+        var firstAccount = storage.getById(1)
+                .orElseThrow(() -> new IllegalStateException("Not found account by id = 1"));
+        assertThat(firstAccount.amount()).isEqualTo(100);
+    }
+
+    @Test
+    void whenUpdate() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 100));
+        storage.update(new Account(1, 200));
+        var firstAccount = storage.getById(1)
+                .orElseThrow(() -> new IllegalStateException("Not found account by id = 2"));
+        assertThat(firstAccount.amount()).isEqualTo(200);
+    }
+
+    @Test
+    void whenDelete() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 100));
+        storage.delete(1);
+        assertThat(storage.getById(1)).isEmpty();
+    }
+
+    @Test
+    void whenTransfer() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 100));
+        storage.add(new Account(2, 100));
+        storage.transfer(1, 2, 100);
+        var firstAccount = storage.getById(1)
+                .orElseThrow(() -> new IllegalStateException("Not found account by id = 1"));
+        var secondAccount = storage.getById(2)
+                .orElseThrow(() -> new IllegalStateException("Not found account by id = 2"));
+        assertThat(firstAccount.amount()).isEqualTo(0);
+        assertThat(secondAccount.amount()).isEqualTo(200);
+    }
+
+    @Test
+    void whenTransferWithException() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 100));
+        storage.add(new Account(2, 100));
+        assertThatThrownBy(() -> storage.transfer(5, 6, 100))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("Not found account by id = 5");
+    }
+
+    @Test
+    void whenTransferUnSuccess() {
+        var storage = new AccountStorage();
+        storage.add(new Account(5, 200));
+        storage.add(new Account(4, 300));
+        storage.transfer(5, 4, 500);
+        assertThat(storage.transfer(5, 4, 500)).isFalse();
+    }
+}
