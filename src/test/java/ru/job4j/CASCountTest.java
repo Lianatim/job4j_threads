90c653763cf29ad9e03848fa96aa07c7529637af
Liana <95958723+Lianatim@users.noreply.github.com>
2022-11-11 21:30:23 +0500

    0. CAS - операции

--- src/test/java/ru/job4j/CASCountTest.java
+++ src/test/java/ru/job4j/CASCountTest.java
@@ -7,12 +7,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CASCountTest {
 
     @Test
-    public void whenExecute2ThreadThen2() {
+    public void whenIncrementAndGet() {
         CASCount casCount = new CASCount();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 150; i++) {
             new Thread(casCount::increment).start();
         }
-        assertThat(casCount.get()).isEqualTo(99);
+        assertThat(casCount.get()).isEqualTo(149);
+    }
+
+    @Test
+    public void whenIncrementAndGetTwoThread() {
+        CASCount casCount = new CASCount();
+        Thread thread = new Thread(() -> {
+                for (int j = 1; j <= 150; j++) {
+                    casCount.increment();
+                }
+            });
+        Thread thread2 = new Thread(() -> {
+            for (int j = 1; j <= 150; j++) {
+                casCount.increment();
+            }
+        });
+        thread.start();
+        thread2.start();
+        try {
+            thread.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertThat(casCount.get()).isEqualTo(300);
     }
 
 }
