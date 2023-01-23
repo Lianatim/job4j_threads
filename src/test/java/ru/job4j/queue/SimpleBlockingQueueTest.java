e681ca95aeac7cd3ad0da943b951c754b82246e3
Liana <95958723+Lianatim@users.noreply.github.com>
2023-01-23 22:43:09 +0300

    3.2. Откат изменений

--- src/test/java/ru/job4j/queue/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/queue/SimpleBlockingQueueTest.java
@@ -34,7 +34,6 @@ class SimpleBlockingQueueTest {
                         try {
                             buffer.add(queue.poll());
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
                             Thread.currentThread().interrupt();
                         }
                     }
